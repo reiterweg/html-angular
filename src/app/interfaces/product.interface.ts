fd3045de706fe87d0d940fa99e9f850b583d4485
Alejandro Diaz <reiterweg@gmail.com>
2018-10-01 00:21:06 -0500

    Refactor of the product images folder.

---
+++ src/app/interfaces/product.interface.ts
@@ -0,0 +1,9 @@
+export interface Product {
+  title?: string;
+  category?: string;
+  description1?: string;
+  description2?: string;
+  subtitle1?: string;
+  subtitle2?: string;
+  summary?: string;
+}
